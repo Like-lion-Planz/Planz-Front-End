531a4bfc6030f52d84b8fe0ef056eb1d8d59134a
mstagon <devcms21@gmail.com>
2024-07-30 22:45:31 +0900

    sociallogin

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:planz/screen/home/schedule.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:planz/screen/home/testhome.dart';
 
 void main() async{
   await initializeDateFormatting();
